e9939ae02039f0b50a9710ebf2d5b0104238fbd1
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-15 23:30:12 +0900

    [updata]DayaType comment

--- DataType.java
+++ DataType.java
@@ -2,10 +2,10 @@ class DataType {
     public static void main(String[] args) {
         int month = 12;
         int day = 29;
-        float weight = 63.0f;
-        double height = 168.5;
-        char bloodType = 'A';
-        String name = "テスト";
+        float weight = 63.0f; // float型の少数ははfをつける
+        double height = 168.5; // double型も少数ははfをつける
+        char bloodType = 'A'; // char型の文字は'で囲む
+        String name = "テスト"; // そのまま「テスト」の文字列が入るのではなく文字列のアドレスが入る
 
         System.out.println("こんにちは！" + name + "です。");
         System.out.println("身長は" + height + "cm、体重は" + weight + "kg、");
