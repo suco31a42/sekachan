efa1225bc07416d70cb6e040a6a19303db7d803e
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-15 22:40:35 +0900

    "[add]Variable.java"

---
+++ FirstComment.java
@@ -0,0 +1,8 @@
+/* FirstComment.java
+ * 文字列を表示するプログラム
+　2023.9.15　作成 */
+class FirstComment {
+    public static void main(String[] args) {
+        System.out.println("Javaを楽しもう"); // 画面表示
+    }
+}
